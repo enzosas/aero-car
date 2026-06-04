0d65350be75b338ed541936c4ce4ea89ca75dafe
Enzo Silveira <enzo.silveira2@gmail.com>
2026-06-04 19:32:17 -0300

    permitir ver de longe

--- src/App.jsx
+++ src/App.jsx
@@ -219,7 +219,7 @@ export default function App() {
       </div>
 
       <div style={{ width: '100vw', height: '100vh', margin: 0, overflow: 'hidden' }}>
-        <Canvas camera={{ position: [-400, 120, 400], fov: 60 }}>
+        <Canvas camera={{ position: [-400, 120, 400], fov: 60, near: 0.1, far: 15000 }}>
           <FundoImagem />
           <ambientLight intensity={0.5} />
           <directionalLight position={[100, 200, 100]} />
